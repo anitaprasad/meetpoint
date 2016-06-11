ae6692251276578f1da23516ba611a7495ac2fa2
Anita Prasad <aprasad2017@kellogg.northwestern.edu>
2016-06-11 14:47:37 -0700

    Input for different categories
    Updated to include input for different categories

--- app/controllers/meetpoint_controller.rb
+++ app/controllers/meetpoint_controller.rb
@@ -11,7 +11,8 @@ class MeetpointController < ApplicationController
 
     @address_1 = params[:user_address_1]
     @address_2 = params[:user_address_2]
-    
+    @category=params[:category]
+
     if params[:user_address_1]=="" or params[:user_address_2]==""
       @missing_address_1=params[:user_address_1]==""
       @missing_address_2=params[:user_address_2]==""
@@ -38,7 +39,7 @@ class MeetpointController < ApplicationController
       @mid_latitude = (@latitude_1 + @latitude_2)/2
       @mid_longitude = (@longitude_1 + @longitude_2)/2
 
-      params = { term: 'food', limit: 5}
+      params = { term: @category, limit: 5}
       coordinates = {latitude: @mid_latitude, longitude: @mid_longitude}
 
       @yelp_biz = Yelp.client.search_by_coordinates(coordinates, params).businesses
